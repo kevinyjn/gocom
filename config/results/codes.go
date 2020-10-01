4fa1e618450593b5f2e1af86dfd80562029995f9
kevinyjn <kevinyjn@foxmail.com>
2020-10-01 22:44:51 +0800

    add some result codes

--- config/results/codes.go
+++ config/results/codes.go
@@ -5,11 +5,16 @@ const (
 	Failed                 = -1
 	OK                     = 0
 	SUCCESS                = 0
-	InvalidInput           = 101
 	Unauthorized           = 401
-	UnknownOrgnazationCode = 102
-	ServiceNotSupported    = 201
-	VerifyFailed           = 204
-	DataNotExists          = 205
+	PaymentRequired        = 402
+	Forbidden              = 403
 	InnerError             = 500
+	NotImplemented         = 501
+	ServiceUnavailable     = 503
+	Timeout                = 504
+	ServiceNotSupported    = 701
+	VerifyFailed           = 704
+	DataNotExists          = 705
+	InvalidInput           = 1101
+	UnknownOrgnazationCode = 1102
 )
